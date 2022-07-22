0bd73d73310ddcb1dca0a4dbbc6eefd74edc3d40
buiquangvinh9a4 <boydeptray18042001@gmail.com>
2022-07-22 15:31:58 +0700

    update quiz ver.1

---
+++ js/js-answer.js
@@ -0,0 +1,8 @@
+var result = document.getElementById('js-result');
+var back_opacity = document.getElementById('js-back-opacity');
+
+function back() {
+    var btn_back = document.getElementById('js-back');
+    result.style.display = 'none';
+    back_opacity.style.backgroundColor = 'rgb(240, 240, 240)';
+}
